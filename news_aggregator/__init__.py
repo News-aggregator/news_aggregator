0a321b3eaf0de2142cab23dea88f3c4a03a24137
msweet168 <mitchellsweet168@aol.com>
2018-02-17 14:53:37 -0500

    Added title bar and grid layout.

--- news_aggregator/__init__.py
+++ news_aggregator/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('map.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0')

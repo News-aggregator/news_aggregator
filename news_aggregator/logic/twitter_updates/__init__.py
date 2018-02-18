4bddca373945ae2cbffb1acfd3e69fe8a4720d5e
Seth Gower <seth@sethgower.com>
2018-02-18 01:48:36 -0500

    working on sending json data from twitter

--- news_aggregator/logic/twitter_updates/__init__.py
+++ news_aggregator/logic/twitter_updates/__init__.py
@@ -1,20 +1,39 @@
 from __future__ import absolute_import, print_function
-import configparser, logging
 
+from flask import Flask, request, jsonify
+
+import configparser, logging
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import tweepy
+import requests
+
+from time import sleep
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return 'Hello Fuck Face'
+
+@app.route("/twitter", methods=['POST'])
+def scrape_json():
+    print(request.is_json)
+    #if request.is_json():
+    #    print('Received JSON:')
+    #    print(request.get_json())
+    #else:
+    #    print('NO FUCKING JSON')
+    return 'fuck you'
 
 class StdOutListener(StreamListener):
-    #def on_data(self, data):
-    #    print(data)
-    #    return True
+    def on_data(self, data):
+        r = requests.post('http://127.0.0.1:5000/twitter', data = {'key':'value'})
+        return True
     def on_error(self, status):
-         print(status)
-    def on_status(self, status):
-        print(status.text)
-
+        print(status)
+    
 class TwitterUpdate():
     consumer_key=""
     consumer_secret=""
@@ -24,8 +43,6 @@ class TwitterUpdate():
     
     config = configparser.ConfigParser()
     config.read('tweet.ini')
-    
-    print(config.sections()) 
     
     if 'twitter' in config.sections():
         consumer_key = config['twitter']['consumerKey']
@@ -37,16 +54,12 @@ class TwitterUpdate():
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     
-    
-    
     api = tweepy.API(auth)
-    
-    
 
     listener = StdOutListener()
 
     stream = Stream(auth, listener)
-    stream.filter(track=['RIT'], async=True)
+    stream.filter(track=['RIT Tigers'], async=True)
 
 if __name__ == '__main__':
-    twitter = TwitterUpdate()
+    main()

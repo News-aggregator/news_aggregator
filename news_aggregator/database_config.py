fd85694df6320c2e95ddcf66002d875b6e0806f2
SkylerJaneClark <puffballinthedark@gmail.com>
2018-02-17 21:17:40 -0500

    splits up init.py and starts to add some rss reading

---
+++ news_aggregator/database_config.py
@@ -0,0 +1,52 @@
+from sqlalchemy import ForeignKey
+from news_aggregator.engine import *
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = engine
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+db = SQLAlchemy(app)
+
+
+class country(db.Model):
+    country = db.Column(db.String(40))
+    country_iso2 = db.Column(db.String(2))
+    country_iso3 = db.Column(db.String(3))
+    country_iso_num = db.Column(db.Integer, primary_key = True)
+    def __init__(self, country, country_iso2, country_iso3, country_iso_num):
+        self.country = country
+        self.country_iso2 = country_iso2
+        self.country_iso3 = country_iso3
+        self.country_iso_num = country_iso_num
+class alias (db.Model):
+    country_alias =db.Column(db.String(40), primary_key = True)
+    country_iso_num = db.column(ForeignKey("country.country_iso_num"))
+    def __init__ (self, country_alias, country_iso_num):
+        self.country_alias = country_alias
+        self.country_iso_num = country_iso_num
+class state (db.Model):
+    state = db.Column(db.String(40))
+    state_code = db.Column(db.String(40), primary_key = True)
+    def __init__ (self, state, state_code):
+        self.state = state
+        self.state_code = state_code
+class cities (db.Model):
+    city = db.Column(db.String(30))
+    city_id = db.Column(db.String(6), primary_key = True)
+    state_code = db.Column(ForeignKey("state.state_code"))
+    def __init__(self, city, city_id):
+        self.city = city
+        self.city_id = city_id
+class news (db.Model):
+    url = db.Column(db.String(500), primary_key = True)
+    country_iso_num = db.Column(db.Integer)
+    state_code = db.Column(db.String(2))
+    city_id = db.Column(db.Integer)
+    def __init__ (self, url, country_iso_num, state_code, city_id):
+        self.url = url
+        self.country_iso_num = country_iso_num
+        self.state_code = state_code
+        self.city_id = city_id
---
+++ news_aggregator/logic/parseRSS.py
@@ -0,0 +1,18 @@
+import feedparser
+import os
+import json
+
+
+cnn_worldnews = feedparser.parse('http://rss.cnn.com/rss/cnn_world.rss')
+
+def populate_db():
+    file = open('location_data/all.csv')
+
+    for line in file:
+        print(line.split(",")[3])
+    print(file);
+
+def loop_through_news(news):
+    print(news)
+
+populate_db()
